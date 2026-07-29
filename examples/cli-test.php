fc4a7310364c14cb7baa037cd91a87330821c0be
UOFI\hualiu6 <hualiu6@uic.edu>
2026-07-29 12:17:39 -0500

    MNTC-1827 #time 19m [dev] Update  pacakage case

--- examples/cli-test.php
+++ examples/cli-test.php
@@ -27,7 +27,7 @@ try {
     }
 
     // Call the AITS Term API
-    $gradesAPI = new dpazuic\aits_grades($argv[2], $argv[1], empty($argv[3]) ? null : $argv[3]);
+    $gradesAPI = new Uicosss\aits_grades($argv[2], $argv[1], empty($argv[3]) ? null : $argv[3]);
 
     // Get the results of a call
     $gradesAPI->getAITSGrades();
